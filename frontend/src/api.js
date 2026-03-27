47d862a7fb732df4f6e60d4bc90c817db802d9c3
ANTHONY sifuna <www.antonysifuna07@gmail.com>
2026-03-28 02:08:19 +0300

    Fix doubled /api in URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || 'https://school-management-system-for-cbc-and.onrender.com';
 
 const api = axios.create({
-  baseURL: API_URL ? `${API_URL}/api` : '/api',
+  baseURL: API_URL ? API_URL : '/api',
 });
 
 export default api;
